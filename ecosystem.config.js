161a821a8b0c30e3b3495dea8d4fac8635c553f5
ronnachait <catoonxpzero@gmail.com>
2025-09-27 17:38:23 +0700

    test 002

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "nextjs",
+      script: "./server.js", // มาจาก .next/standalone
+      env: {
+        NODE_ENV: "production",
+        PORT: process.env.PORT || 3000, // Azure จะ set PORT ให้อัตโนมัติ
+        DATABASE_URL: process.env.DATABASE_URL,
+        PI_API_KEY: process.env.PI_API_KEY,
+      },
+    },
+    {
+      name: "ws-server",
+      script: "./ws-server.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: process.env.WS_PORT || 8090, // ถ้าอยาก fix port
+      },
+    },
+  ],
+};
