6b3d1938df55c254bad9e1014b842e5bcfca9429
ronnachait <catoonxpzero@gmail.com>
2025-09-27 16:38:16 +0700

    test001

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,32 @@
-declare module "next/dist/compiled/webpack/webpack";
-declare module "next/dist/compiled/amphtml-validator";
-declare module "next/dist/compiled/superstruct";
+// global.d.ts
+
+declare module "next/dist/compiled/webpack/webpack" {
+  import * as webpack from "webpack";
+  export = webpack;
+}
+
+declare module "next/dist/compiled/superstruct" {
+  export * from "superstruct";
+}
+
+declare module "next/dist/compiled/amphtml-validator" {
+  export interface ValidationError {
+    severity: string;
+    line: number;
+    col: number;
+    message: string;
+    specUrl: string;
+  }
+
+  export interface ValidationResult {
+    status: "PASS" | "FAIL";
+    errors: ValidationError[];
+  }
+
+  export interface Validator {
+    validateString(html: string): ValidationResult;
+    validateUrl(url: string): Promise<ValidationResult>;
+  }
+
+  export function getInstance(): Promise<Validator>;
+}
